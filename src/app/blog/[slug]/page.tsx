8c8e1e76c47a45552814a1af4e990bbf480b9746
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2025-01-04 12:14:45 +0500

    update

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
+// import { components } from "@/components/CustomComponent";
 import { client } from "@/sanity/lib/client";
 import { urlForImage } from "@/sanity/lib/image";
-import { components } from "@/app/components/CustomComponent";
 
 export const revalidate = 60; //seconds
 
@@ -10,6 +10,7 @@ export const revalidate = 60; //seconds
   const query = `*[_type=='post']{
     "slug":slug.current
   }`;
+
   const slugs = await client.fetch(query);
   const slugRoutes = slugs.map((item:{slug:string})=>(
     item.slug
@@ -18,7 +19,7 @@ export const revalidate = 60; //seconds
   return slugRoutes.map((slug:string)=>(
     {slug}
   ))
-  
+
 }
 
 // To create static pages for dynamic routes
@@ -30,6 +31,8 @@ export default async function page({params:{slug}}:{params:{slug:string}}) {
   }[0]`;
   const post = await client.fetch(query);
   // console.log(post);
+
+
 
   return (
     <article className="mt-12 mb-24 px-2 2xl:px-12 flex flex-col gap-y-8">
@@ -48,7 +51,7 @@ export default async function page({params:{slug}}:{params:{slug:string}}) {
         className="rounded"
       />
 
-      {/* Blog Summary Section */}
+{/* Blog Summary Section */}
       <section>
       <h2 className="text-xl xs:text-2xl md:text-3xl font-bold uppercase text-accentDarkPrimary">
         Summary
@@ -58,14 +61,35 @@ export default async function page({params:{slug}}:{params:{slug:string}}) {
       </p>
       </section>
 
+      {/* Author Section (Image & Bio) */}
+      <section className="px-2 sm:px-8 md:px-12 flex gap-2 xs:gap-4 sm:gap-6 items-start xs:items-center justify-start">
+        <Image
+          src={urlForImage(post.author.image)}
+          width={200}
+          height={200}
+          alt="author"
+          className="object-cover rounded-full h-12 w-12 sm:h-24 sm:w-24"
+        />
+        <div className="flex flex-col gap-1">
+          <h3 className="text-xl font-bold text-dark dark:text-light">{post.author.name}</h3>
+          <p className="italic text-xs xs:text-sm sm:text-base text-dark/80 dark:text-light/80">
+            {post.author.bio}
+          </p>
+        </div>
+      </section>
 
       {/* Main Body of Blog */}
-      <section className="text-lg leading-normal text-dark/80 dark:text-light/80">
+      <section className="text-lg leading-normal text-dark/80 dark:text-light/80
+      prose-h4:text-accentDarkPrimary prose-h4:text-3xl prose-h4:font-bold
+      prose-li:list-disc prose-li:list-inside prose-li:marker:text-accentDarkSecondary
+      prose-strong:text-dark dark:prose-strong:text-white
+
+      ">
         <PortableText 
         value={post.content} 
-        components={components} 
+        // components={components} 
         />
-        
+
       </section>
     </article>
   );
